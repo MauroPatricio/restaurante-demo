2e8088685b3326a571e403477860fce74d25e4f8
Mauro Patricio <mpatricio@cedsif.gov.mz>
2026-03-12 17:14:45 +0200

    Actualizacao

--- qr-menu/admin-dashboard/vite.config.js
+++ qr-menu/admin-dashboard/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
+		maximumFileSizeToCacheInBytes: 5000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,woff,ttf}'],
         runtimeCaching: [
           {
